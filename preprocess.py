81782e24e4e8c273d47ce8a33049d41b9e8b6e7d
Aswin Krishna M <maswink@gmail.com>
2020-12-09 15:30:38 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -6,9 +6,13 @@ import demoji
 from nltk.stem.wordnet import WordNetLemmatizer
 
 class preprocess():
-    def __init__(self, df, contractions):
+    def __init__(self, df, contractions, otherContractions):
         self.df = df
         self.contractions = contractions
+        self.otherContractions = otherContractions
+    
+    def lower(self, tweet):
+        return tweet.lower()
     
     def abbreviate(self, tweet):
         tweet = tweet.split(' ')
@@ -30,6 +34,8 @@ class preprocess():
         for word in tweet.split():
             if word in self.contractions.keys():
                 tweet = tweet.replace(word, self.contractions[word])
+            elif word in self.otherContractions.keys():
+                tweet = tweet.replace(word, self.otherContractions[word])
         return tweet
     
     def emoji2text(self, tweet):
@@ -45,20 +51,15 @@ class preprocess():
                     tweet = tweet.replace(word, wordmojis[char])
         
         return tweet
-    
-    def normalize(self, tweet):
-        lem = WordNetLemmatizer()
-        normalized_tweet = []
-        for word in tweet.split():
-            normalized_text = lem.lemmatize(word, 'v')
-            normalized_tweet.append(normalized_text)
-        return ' '.join(normalized_tweet)
 
-    def remove_mentions(self, tweet):
-        return re.sub(r'\@w+', '', tweet)
-    
-    def remove_hashtag(self, tweet):
+    def remove_hashtags(self, tweet):
         return re.sub(r'\#w+', '', tweet)
+    
+    def remove_mentions(self, tweet):
+        for word in tweet.split():
+            if word[0] == '@':
+                tweet = tweet.replace(word, '')
+        return tweet
 
     def remove_punctuations(self, tweet):
         punct = string.punctuation
@@ -66,11 +67,11 @@ class preprocess():
         return tweet.translate(trantab)
     
     def preprocess_tweet(self, tweet):
+        tweet = self.lower(tweet)
         tweet = self.abbreviate(tweet)
         tweet = self.expand(tweet)
         tweet = self.emoji2text(tweet)
-        tweet = self.normalize(tweet)
         tweet = self.remove_mentions(tweet)
-        tweet = self.remove_hashtag(tweet)
+        tweet = self.remove_hashtags(tweet)
         tweet = self.remove_punctuations(tweet)
         return tweet
